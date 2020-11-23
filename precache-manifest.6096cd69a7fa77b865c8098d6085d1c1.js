12f4eece0d403fa32594b85bfad53decdeaa23f2
Swarnim Gupta <swarnimgupta2000@gmail.com>
2020-11-23 10:42:52 +0530

    Updates

---
+++ precache-manifest.6096cd69a7fa77b865c8098d6085d1c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "403872149c213e13c56692766e128719",
+    "url": "/Robofriends_Webapp/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/Robofriends_Webapp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "60b6cc6d5a7bf3d81673",
+    "url": "/Robofriends_Webapp/static/css/main.a2330957.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/Robofriends_Webapp/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robofriends_Webapp/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60b6cc6d5a7bf3d81673",
+    "url": "/Robofriends_Webapp/static/js/main.52fb5b58.chunk.js"
+  },
+  {
+    "revision": "42200aa9df1a218aee6f",
+    "url": "/Robofriends_Webapp/static/js/runtime-main.8bca40a5.js"
+  }
+]);
