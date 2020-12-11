b6e41f57bcc2faa176cdd3a6daa6e8bc66aff8ab
Swarnim Gupta <swarnimgupta2000@gmail.com>
2020-12-11 18:03:29 +0530

    Redux 2

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,40 +1,41 @@
-import React ,{ Component }from 'react';
+import React ,{ Component,useEffect }from 'react';
 import {connect} from 'react-redux';
 import CardList from '../components/CardList';
-import { robot } from '../robot';
 import Searchbox from '../components/Searchbox';
-import { setSearchField } from '../action';
+import { requestRobots, setSearchField } from '../action';
 
 const mapStateToProps = state => {
     return{
-            searchField:state.searchField
+            searchField:state.searchRobots.searchField,
+            robots:state.requestRobots.robots,
+            isPending:state.requestRobots.isPending,
+            error : state.requestRobots.error
+
     }
 }
 const mapDispatchToProps = (dispatch) => {
-    return {onSearchChange : (e) => {
-        dispatch(setSearchField(e.target.value))
-    }}
-}
-
-class App extends Component{
-    constructor()
-    {
-        super()
-        this.state = {
-            robot: robot,
-        }
+    return {
+        onSearchChange : (e) => dispatch(setSearchField(e.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
     }
+}
+class App extends Component{
+componentDidMount(){
+    this.props.onRequestRobots();
+}
     render(){
-        const {onSearchChange,searchField} = this.props;
-        const filteredrobots = this.state.robot.filter(robot => {
+        const {onSearchChange,searchField,robots,isPending} = this.props;
+        const filteredrobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchField.toLowerCase());
         })
-        return(<div className = 'tc'>
+        return(
+            isPending ? <h1 style = {{display:"flex",justifyContent:"center",alignItems:"center"}}>Loading</h1> :
+        (<div className = 'tc'>
             <h1>Robofriends</h1>
         <Searchbox searchChange = {onSearchChange} />
          <div style = {{overflowY:"scroll" , height : "610px", border: "1px solid black"}}>
         <CardList robot = {filteredrobots}/></div>
-        </div>);
+        </div>));
     }
 }
 export default connect(mapStateToProps , mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore,applyMiddleware ,combineReducers} from 'redux';
+import {createLogger} from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import App from './containers/App';
-import { searchRobots } from './reducer';
+import { searchRobots,requestRobots } from './reducer';
 
-const store = createStore(searchRobots);
+const logger = createLogger();
+
+const rootReducer = combineReducers({searchRobots,requestRobots})
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware,logger));
 
 ReactDOM.render(
   <Provider store = {store}  >
